bee4f6e908fa289859d74526328eeb102a505a78
Josef Jura <josef.jura@hotmail.com>
2018-12-15 23:34:12 +0100

    Duuude!

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,43 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.parseFile = parseFile;
+exports.parse = parse;
+
 var _fs = require("fs");
 
 var _classes = require("./classes");
 
 function parseFile(uri) {
-  (0, _fs.readFile)(uri, {
-    encoding: "utf8"
-  }, function (err, data) {
-    if (!err) {
-      return parse(data);
-    }
+  return new Promise(function (resolve, reject) {
+    (0, _fs.readFile)(uri, {
+      encoding: "utf8"
+    }, function (err, data) {
+      if (err) reject(err);else resolve(parse(data));
+    });
   });
 }
 
 function parse(text) {
-  console.log(text);
-  var test = new _classes.ABOHeaderParser();
+  var result = [];
+  var ahp = new _classes.ABOHeaderParser();
+  var ghp = new _classes.GroupHeaderParser();
+  var fhp = new _classes.FileHeaderParser();
+  var rp = new _classes.RecordParser();
+  var rows = text.split('\r\n');
+  ahp.parse(rows[0]);
+  result.push(ahp.rawData);
+  ghp.parse(rows[1]);
+  result.push(ghp.rawData);
+  fhp.parse(rows[2]);
+  result.push(fhp.rawData);
+  rp.parse(rows[3]);
+  result.push(rp.rawData);
+  return result;
 }
 
-parseFile("./examples/example1.KPC");
+parseFile("./examples/example1.KPC").then(function (data) {
+  console.log(data);
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import { readFile } from 'fs'
-import { ABOHeaderParser } from './classes'
+import { ABOHeaderParser, GroupHeaderParser, FileHeaderParser, RecordParser } from './classes'
 
-function parseFile(uri) {
-    readFile(uri, { encoding: "utf8" }, (err, data) => {
-        if (!err) {
-            return parse(data);
-        }
+export function parseFile(uri) {
+    return new Promise((resolve, reject) => {
+        readFile(uri, { encoding: "utf8" }, (err, data) => {
+            if (err)
+                reject(err);
+            else
+                resolve(parse(data));
+
+        })
     })
 }
 
-function parse(text) {
-    console.log(text);
-    var test = new ABOHeaderParser();
+export function parse(text) {
+var result = [];
+
+    var ahp = new ABOHeaderParser();
+    var ghp = new GroupHeaderParser();
+    var fhp = new FileHeaderParser();
+    var rp = new RecordParser();
+    var rows = text.split('\r\n');
+    ahp.parse(rows[0]);
+    result.push(ahp.rawData);
+    ghp.parse(rows[1]);
+    result.push(ghp.rawData);
+    fhp.parse(rows[2]);
+    result.push(fhp.rawData);
+    rp.parse(rows[3]);
+    result.push(rp.rawData);
+
+    return result;
 }
 
-parseFile("./examples/example1.KPC");
+parseFile("./examples/example1.KPC").then((data) => {
+    console.log(data);
+});
